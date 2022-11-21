4b82b06e57d1d9763a0abf1eb82246cecb414c7e
Matheus Gomes <7kiks7@gmail.com>
2022-11-20 23:20:12 -0300

    Notificacão que foi enviado

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,13 +2,14 @@ import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
 import "./styles.css";
 import axios from "axios";
+import { toast } from "react-toastify";
 
 type Props = {
   saleId: number;
 };
 function handleClick(id: number) {
   axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
-    console.log("Sucesso");
+    toast.info("SMS enviado com sucesso");
   });
 }
 
