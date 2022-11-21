81c41f3abc7a15f4f0c48283ae7998e43712c3de
Matheus Gomes <7kiks7@gmail.com>
2022-11-20 23:16:52 -0300

    Enviar notificação funcional

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,23 @@
-import icon from '../../assets/img/notification-icon.svg'
-import './styles.css'
+import icon from "../../assets/img/notification-icon.svg";
+import { BASE_URL } from "../../utils/request";
+import "./styles.css";
+import axios from "axios";
 
-function NotificationButton() {
-  return(
-    <div className="dsmeta-red-btn">
-      <img src={icon} alt="notificar" />
-    </div>
-  )
+type Props = {
+  saleId: number;
+};
+function handleClick(id: number) {
+  axios(`${BASE_URL}/sales/${id}/notification`).then((response) => {
+    console.log("Sucesso");
+  });
 }
 
-export default NotificationButton
+function NotificationButton({ saleId }: Props) {
+  return (
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+      <img src={icon} alt="notificar" />
+    </div>
+  );
+}
+
+export default NotificationButton;
